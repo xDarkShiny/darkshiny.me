d11148d7af273f8ead7d91f699c1cb3822bb076a
DarkShiny <69814650+xDarkShiny@users.noreply.github.com>
2022-03-06 23:23:27 +0300

    🎉New Page

--- languages.php
+++ languages.php
@@ -69,6 +69,47 @@ require_once 'companenets/discord.php';
     </style>
 </head>
 <body class="bg-black p-10">
+        <?php
+        include 'companenets/navbar.php';
+    ?>
+        <a class="flex text-5xl mt-20 text-style font-bold text-end text-green-500">Languages | I Use</a>
+
+
+        <div class="grid mt-20 p-10 grid-cols-2 sm:grid-cols-12 gap-4">
+            <a class="flex flex-col items-center space-y-3">
+                <img class="w-32 bg-green-500 bg-opacity-30 rounded-2xl p-2 h-32" src="https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/vue-dot-js.svg">
+                <h1 class="text-4xl text-green-900 text-style font-bold">Vue</h1>
+            </a>
+            <a class="flex flex-col items-center space-y-3">
+                <img class="w-32 bg-blue-500 bg-opacity-30 rounded-2xl p-2 h-32" src="https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/react.svg">
+                <h1 class="text-4xl text-blue-900 text-style font-bold">React</h1>
+            </a>
+            <a class="flex flex-col items-center space-y-3">
+                <img class="w-32 bg-purple-500 bg-opacity-30 rounded-2xl p-2 h-32" src="https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/php.svg">
+                <h1 class="text-4xl text-purple-900 text-style font-bold">Php</h1>
+            </a>
+            <a class="flex flex-col items-center space-y-3">
+                <img class="w-32 bg-blue-700 bg-opacity-30 rounded-2xl p-2 h-32" src="https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/tailwindcss.svg">
+                <h1 class="text-4xl text-blue-900 text-style font-bold">T.Css</h1>
+            </a>
+            <a class="flex flex-col items-center space-y-3">
+                <img class="w-32 bg-orange-700 bg-opacity-30 rounded-2xl p-2 h-32" src="https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/html5.svg">
+                <h1 class="text-4xl text-orange-900 text-style font-bold">Html</h1>
+            </a>
+            <a class="flex flex-col items-center space-y-3">
+                <img class="w-32 bg-purple-900 bg-opacity-30 rounded-2xl p-2 h-32" src="https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/adobexd.svg">
+                <h1 class="text-4xl text-purple-900 text-style font-bold">Html</h1>
+            </a>
+            <a class="flex flex-col items-center space-y-3">
+                <img class="w-32 bg-red-900 bg-opacity-30 rounded-2xl p-2 h-32" src="https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/fontawesome.svg">
+                <h1 class="text-4xl text-red-900 text-style font-bold">Font.a</h1>
+            </a>
+            <a class="flex flex-col items-center space-y-3">
+                <img class="w-32 bg-sky-900 bg-opacity-30 rounded-2xl p-2 h-32" src="https://cdn.jsdelivr.net/npm/simple-icons@3.13.0/icons/github.svg">
+                <h1 class="text-4xl text-sky-900 text-style font-bold">Github</h1>
+            </a>
+
+        </div>
 
 <div class="fixed bottom-0 right-0 p-5">
     <button onclick="topFunction()" id="myBtn" class="rounded-full  w-10 h-10 bg-green-600"><i class="fa-solid fa-angles-up"></i></button>
